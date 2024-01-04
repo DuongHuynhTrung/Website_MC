c44d4c7eda81331ad858d11d887ec80e1d251bf3
Duong Huynh <trungduong22021619@gmail.com>
2024-01-04 22:20:34 +0700

    Add swagger

--- src/user-chat/user-chat.controller.ts
+++ src/user-chat/user-chat.controller.ts
@@ -1,22 +1,38 @@
-import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 import { UserChatService } from './user-chat.service';
 import { CreateUserChatDto } from './dto/create-user-chat.dto';
 import { UpdateUserChatDto } from './dto/update-user-chat.dto';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { JwtGuard } from 'src/auth/jwt.guard';
 
+@ApiTags('User Chat')
+@UseGuards(JwtGuard)
+@ApiBearerAuth()
 @Controller('user-chat')
 export class UserChatController {
   constructor(private readonly userChatService: UserChatService) {}
 
+  @ApiOperation({ summary: 'Create a new User-Chat' })
   @Post()
   createUserChat(@Body() createUserChatDto: CreateUserChatDto) {
     return this.userChatService.createUserChat(createUserChatDto);
   }
 
+  @ApiOperation({ summary: 'Get All User-Chat' })
   @Get(':identifierUserChat')
   getAllUserChats(@Param('identifierUserChat') identifierUserChat: string) {
     return this.userChatService.getAllUserChats(identifierUserChat);
   }
 
+  @ApiOperation({ summary: 'Update User-Chat' })
   @Patch(':identifierUserChat')
   updateUserChatsByIdentifier(
     @Param('identifierUserChat') identifierUserChat: string,
